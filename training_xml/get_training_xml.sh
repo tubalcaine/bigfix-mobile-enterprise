40959dc350e3435f3dc9033078c6457668ef193d
Michael Schwarz <mschwarz@multitool.net>
2023-11-14 17:00:55 -0600

    Add items to training doc generating shell script

--- training_xml/get_training_xml.sh
+++ training_xml/get_training_xml.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/computers > api_computers.xml
+curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/computer/9883517 > api_computer_9883517.xml
+curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/computer/13419998 > api_computer_13419998.xml
+curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/actions > api_actions.xml
 
