de26e47c7c81b3dca092d776780a015a17080af8
Michael Schwarz <mschwarz@multitool.net>
2023-11-14 17:37:57 -0600

    Start putting meat on the bones

--- training_xml/get_training_xml.sh
+++ training_xml/get_training_xml.sh
@@ -5,4 +5,4 @@ curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/computer
 curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/actions > api_actions.xml
 curl --insecure -u 'IEMADmin:BigFix!123' https://10.10.220.60:52311/api/action/1267 > api_action_1267.xml
 curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/action/5340 > api_action_5340.xml
-
+curl --insecure -u 'IEMAdmin:BigFix!123' https://10.10.220.60:52311/api/action/5432 > api_action_5432.xml
