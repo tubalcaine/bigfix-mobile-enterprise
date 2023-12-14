a0f74c1f66360f9b70383f42de540acbd723d445
Michael Schwarz <mschwarz@multitool.net>
2023-12-14 08:44:06 -0600

    Fix test file errors

--- pkg/bfrest/test/test_bfcache.go
+++ pkg/bfrest/test/test_bfcache.go
@@ -1,34 +1,29 @@
 package bfrest
 
 import (
+	"sync"
 	"testing"
-)
-package bfrest
-
-import (
-    "testing"
-    "sync"
 )
 
 func TestGetCache(t *testing.T) {
 	resetCache()
-    // Test with non-zero maxAgeSeconds
-    cache := GetCache(500)
-    if cache.maxAge != 500 {
-        t.Errorf("Expected maxAge to be %v, got %v", 500, cache.maxAge)
-    }
+	// Test with non-zero maxAgeSeconds
+	cache := GetCache(500)
+	if cache.maxAge != 500 {
+		t.Errorf("Expected maxAge to be %v, got %v", 500, cache.maxAge)
+	}
 
 	resetCache()
-    // Test with zero maxAgeSeconds
-    cache = GetCache(0)
-    if cache.maxAge != 300 {
-        t.Errorf("Expected maxAge to be %v, got %v", 300, cache.maxAge)
-    }
+	// Test with zero maxAgeSeconds
+	cache = GetCache(0)
+	if cache.maxAge != 300 {
+		t.Errorf("Expected maxAge to be %v, got %v", 300, cache.maxAge)
+	}
 
-    // Test that ServerCache is initialized
-    if _, ok := cache.ServerCache.(*sync.Map); !ok {
-        t.Errorf("Expected ServerCache to be of type *sync.Map, got %T", cache.ServerCache)
-    }
+	// Test that ServerCache is initialized
+	if _, ok := cache.ServerCache.(*sync.Map); !ok {
+		t.Errorf("Expected ServerCache to be of type *sync.Map, got %T", cache.ServerCache)
+	}
 	resetCache()
 }
 
